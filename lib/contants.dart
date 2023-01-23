e09592836cb1866db44915fae7734850e30a5ce3
Chinmay Kalmane <chinmay.flexiple@inai.io>
2023-01-23 16:52:51 +0530

    Changed Country value to USA

--- lib/contants.dart
+++ lib/contants.dart
@@ -2,7 +2,7 @@ class Constants {
   static const String token = "__TOKEN__";
   static const String password = "__PASSWORD__";
   static const String baseUrl = "https://api.inai.io/v1";
-  static const String country = "PAN";
+  static const String country = "USA";
   static const String amount = "1500";
   static const String currency = "USD";
 }
